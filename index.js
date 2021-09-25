dd65cb2ddfb194429fa4dc711238623bbdc0551b
winmonthon <you@example.com>
2021-09-25 23:20:21 +0700

    test10 get userId

--- index.js
+++ index.js
@@ -2,31 +2,25 @@
 
 const line = require("@line/bot-sdk");
 const express = require("express");
-//const taskController = require("./controller/task.controller");
 
 const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message";
 const LINE_HEADER = {
   "Content-Type": "application/json",
   Authorization: `Bearer Q4b+wmv9dBIZxyoM7/vqvfINbBIcrEQShuMCTXZ23ZpOqakgAeVF6RSI9qHQ/aT7LgfFAdWT/7Fp8O44V9bjq5mf7yfk5A4hsQIWOqMNLz7wK2M30MlH47ktf43vxYnPy9p7SgpQRm4hSwVP43rclgdB04t89/1O/w1cDnyilFU=`,
 };
-// create LINE SDK config from env variables
+
 const config = {
   channelAccessToken:
     "Q4b+wmv9dBIZxyoM7/vqvfINbBIcrEQShuMCTXZ23ZpOqakgAeVF6RSI9qHQ/aT7LgfFAdWT/7Fp8O44V9bjq5mf7yfk5A4hsQIWOqMNLz7wK2M30MlH47ktf43vxYnPy9p7SgpQRm4hSwVP43rclgdB04t89/1O/w1cDnyilFU=",
   channelSecret: "bea26cdbe9c077b37770ad69ab939ef4",
 };
 
-// create LINE SDK client
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post("/callback", line.middleware(config), async (req, res) => {
-  await handleEvent(req.body)
+  await handleEvent(req.body.events[0])
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
@@ -34,16 +28,19 @@ app.post("/callback", line.middleware(config), async (req, res) => {
     });
 });
 
-// event handler
-const handleEvent = (bodyResponse) => {
-  // create a echoing text message
-  const echo = { type: "text", text: bodyResponse.events[0].message.text };
-
-  // use reply API
-  return client.replyMessage(bodyResponse.events[0].replyToken, echo);
+const handleEvent = (event) => {
+  if (event.message.text === "get use id") {
+    const payload = {
+      type: "text",
+      text: `${event.source.userId}`,
+    };
+    return client.replyMessage(event.replyToken, payload);
+  } else {
+    const echo = { type: "text", text: event.message.text };
+    return client.replyMessage(event.replyToken, echo);
+  }
 };
 
-// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
