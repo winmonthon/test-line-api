ea855ee58ca43dbb410b8f6e65719ef1c222d140
winmonthon <you@example.com>
2021-09-29 15:33:42 +0700

    test ser33

--- app.js
+++ app.js
@@ -28,33 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Line handle
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-app.post("/callback", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
-
-// event handler
-function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = { type: "text", text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
+app.use("/callback", LineRouter);
 //Task
 app.use("/task", TaskRouter);
 //Users
--- src/modules/Line/lineConfig.js
+++ src/modules/Line/lineConfig.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const lineConfig = {
-  channelAccessToken: process.env.CHANEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 export default lineConfig;
